a96c5a7efde0d2da7d43e242ed3aebcf945081ae
SophieeV <sophievesin@wanadoo.fr>
2021-04-06 11:53:29 +0200

    ajout fichiers

---
+++ script.js
@@ -0,0 +1,15 @@
+function DisplayRed(){
+var ColorRed = document.getElementById('Red');
+var change = document.getElementById('text');
+change.style.color="red";
+}
+function DisplayBlue(){
+    var ColorBlue= document.getElementById('Blue');
+    var change = document.getElementById('text');
+    change.style.color="blue";
+}
+    function DisplayGreen(){
+        var ColorGreen = document.getElementById('Green');
+        var change = document.getElementById('text');
+        change.style.color="green";
+ }
